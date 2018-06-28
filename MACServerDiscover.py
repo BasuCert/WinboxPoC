9633a545c938df1aa86b44252042f042974eb9a1
Lorenzo Santina <lorenzo.santina@gmail.com>
2018-06-28 22:25:43 +0200

    Background discovery

--- MACServerDiscover.py
+++ MACServerDiscover.py
@@ -1,22 +1,34 @@
-import socket, binascii
+import socket, binascii, threading, time
 
 # MAC server discovery by BigNerd95
+
+def discovery(sock):
+    while True:
+        sock.sendto(b"\x00\x00\x00\x00", ("255.255.255.255", 5678))
+        time.sleep(1)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind(("0.0.0.0", 5678))
 
-sock.sendto(b"\x00\x00\x00\x00", ("255.255.255.255", 5678))
+threading.Thread(target=discovery, args=(sock,)).start()
 
 print("Looking for Mikrotik devices (MAC servers)\n")
 
+devices = []
+
 while True:
+    
     data, addr = sock.recvfrom(1024)
     if b"\x00\x01\x00\x06" in data:
         start = data.index(b"\x00\x01\x00\x06") + 4
         mac = data[start:start+6]
-        #print(addr[0])
-        print('\t' + ':'.join('%02x' % b for b in mac))
-        print()
+        
+        if mac not in devices:
+            devices.append(mac)
+
+            #print(addr[0])
+            print('\t' + ':'.join('%02x' % b for b in mac))
+            print()
 
         
