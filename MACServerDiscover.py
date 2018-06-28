d4816e024115beb2adff2018c8919c84b4476f2e
Lorenzo Santina <lorenzo.santina@gmail.com>
2018-06-28 22:39:29 +0200

    kb interrupt

--- MACServerDiscover.py
+++ MACServerDiscover.py
@@ -2,8 +2,12 @@ import socket, binascii, threading, time
 
 # MAC server discovery by BigNerd95
 
+search = True
+devices = []
+
 def discovery(sock):
-    while True:
+    global search
+    while search:
         sock.sendto(b"\x00\x00\x00\x00", ("255.255.255.255", 5678))
         time.sleep(1)
 
@@ -15,20 +19,20 @@ threading.Thread(target=discovery, args=(sock,)).start()
 
 print("Looking for Mikrotik devices (MAC servers)\n")
 
-devices = []
+while search:
+    try:
+        data, addr = sock.recvfrom(1024)
+        if b"\x00\x01\x00\x06" in data:
+            start = data.index(b"\x00\x01\x00\x06") + 4
+            mac = data[start:start+6]
+            
+            if mac not in devices:
+                devices.append(mac)
 
-while True:
-    
-    data, addr = sock.recvfrom(1024)
-    if b"\x00\x01\x00\x06" in data:
-        start = data.index(b"\x00\x01\x00\x06") + 4
-        mac = data[start:start+6]
-        
-        if mac not in devices:
-            devices.append(mac)
-
-            #print(addr[0])
-            print('\t' + ':'.join('%02x' % b for b in mac))
-            print()
-
+                #print(addr[0])
+                print('\t' + ':'.join('%02x' % b for b in mac))
+                print()
+    except KeyboardInterrupt:
+        search = False
+        break
         
